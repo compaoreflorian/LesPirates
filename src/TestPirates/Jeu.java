e0124b1fbfb669a1ae6133dcec932c96f272a01f
Florian Compaore <compaoreeuloge2005@gmail.com>
2026-02-12 12:01:00 +0100

    n

--- src/TestPirates/Jeu.java
+++ src/TestPirates/Jeu.java
@@ -1,15 +1,22 @@
 package TestPirates;
 
-public class Comportement extends JournalOnePiece {
+public class Jeu {
+	private IAffichage journal;
 	private Pion pion1 = new Pion(Couleur.ROUGE);
 	private Pion pion2 = new Pion(Couleur.BLEU);
 	private Plateau plateau1 = new Plateau(pion1, pion2);
 	private Joueur joueur1 = new Joueur("Bill Jambe de Bois", pion2, plateau1);
 	private Joueur joueur2 = new Joueur("ack Le Borne", pion1, plateau1);
+	
+	
+    
+	public Jeu(IAffichage journal) {
+		this.journal = journal;
+	}
 
 	public void initailiserLeJeu() {
-		afficherJoueur(joueur1);
-		afficherJoueur(joueur2);
+		journal.afficherJoueur(joueur1);
+		journal.afficherJoueur(joueur2);
 	};
 
 	public void jouer() {
@@ -19,7 +26,7 @@ public class Comportement extends JournalOnePiece {
 			joueur1.verifierVictoire();
 			joueur1.deplacerJoueur();
 			joueur1.verifierVictoire();
-			boolean avis = afficheravis();
+			boolean avis = journal.afficheravis();
 			if (avis == true) {
 				test = true;
 			} else {
---
+++ src/TestPirates/Main.java
@@ -0,0 +1,9 @@
+package TestPirates;
+
+public class Main {
+	public static void main(String[] args) {
+		IAffichage journal = new JournalOnePiece();
+		Jeu jeu = new Jeu(journal);
+		jeu.initailiserLeJeu();
+	}
+}
